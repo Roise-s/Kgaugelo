ac1fe1a8c6c32bf4cf0c582f7eb4d582355c9f28
Kgaugelo Tladi <163626679+Roise-s@users.noreply.github.com>
2024-11-15 11:19:38 +0200

    added

--- src/component/projects/Projects.tsx
+++ src/component/projects/Projects.tsx
@@ -1,5 +1,6 @@
 import './pro.css'
 import picss1 from '../../assets/Screenshot (67).png'
+import picc1 from '../../assets/matric.png';
 
 
 const Projects = () => {
@@ -7,6 +8,38 @@ const Projects = () => {
     <div className='projects'>
       <p className="protitle">PORTFOLIO</p>
       <p className="litterPro">Each project is a unique piece of development</p>
+
+      <div className="proT">
+        <div className="pro1">
+          <img className='proImgss' src={picc1} alt="Project Picture" />
+        </div>
+        <div className="pro1">
+          <p className="proTitle">EDU</p>
+          <p className="proD">
+            Edu, is a multi-campus public research university. This Website is made to highlight my skills in React working with Vite to improve performance.... and improving my skills with React Scroll
+          </p>
+          <div className="boxss">
+            <button className='btnBoxss'>
+              React with Vite
+            </button>
+            <button className='btnBoxss'>
+               SASS
+            </button>
+            <a href="https://github.com/Roise-s/edu" target='_blank'>
+              <button className='btnBoxss2'>
+                View Code
+              </button>
+            </a>
+            
+            <a href="https://roise-s.github.io/edu/" target='_blank'>
+              <button className='btnBoxss2'>
+                Live Site
+              </button>
+            </a>
+            
+          </div>
+        </div>
+      </div>
 
       <div className="proT">
         <div className="pro1">
